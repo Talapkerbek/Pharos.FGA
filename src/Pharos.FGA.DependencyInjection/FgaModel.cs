04ab95a2893c731a3c46bc350a2250ae029557f0
Islambek <islambekgazizovv@gmail.com>
2025-11-02 01:27:00 +0500

    dd

--- src/Pharos.FGA.DependencyInjection/FgaModel.cs
+++ src/Pharos.FGA.DependencyInjection/FgaModel.cs
@@ -2,41 +2,75 @@
 
 public static class FgaModel
 {
-    public static class Types
+    public static class User
     {
-        public const string User = "user";
-        public const string Platform = "platform";
-        public const string Tenant = "tenant";
-        public const string University = "university";
-        public const string Faculty = "faculty";
+        public const string ObjectName = "user";
     }
 
-    public static string PlatformName = "pharos";
-    
-    public static class PlatformRelations
+    public static class Platform
     {
-        public const string God = "god";
-        public const string Admin = "admin";
+        public const string ObjectName = "platform";
+        public const string ActualObject = "platform:pharos";
+
+        public static class Relations
+        {
+            public const string God = "god";
+            public const string Admin = "admin";
+        }
     }
 
-    public static class TenantRelations
+    public static class Tenant
     {
-        public const string Admin = "admin";
-        public const string Viewer = "viewer";
+        public const string ObjectName = "tenant";
+
+        public static class Relations
+        {
+            public const string Admin = "admin";
+            public const string Viewer = "viewer";
+        }
     }
 
-    public static class UniversityRelations
+    public static class University
     {
-        public const string Admin = "admin";
-        public const string Tenant = "tenant";
-        public const string Viewer = "viewer";
+        public const string ObjectName = "university";
+
+        public static class Relations
+        {
+            public const string Admin = "admin";
+            public const string Tenant = "tenant";
+            public const string Rector = "rector";
+            public const string Prorector = "prorector";
+            public const string Support = "support";
+            public const string Security = "security";
+            public const string Faculty = "faculty";
+        }
     }
 
-    public static class FacultyRelations
+    public static class Faculty
     {
-        public const string Admin = "admin";
-        public const string ProfOrientator = "proforientator";
-        public const string University = "university";
-        public const string Viewer = "viewer";
+        public const string ObjectName = "faculty";
+
+        public static class Relations
+        {
+            public const string Admin = "admin";
+            public const string University = "university";
+            public const string DeputyAdmin = "deputy_admin";
+            public const string Methodist = "methodist";
+        }
+    }
+
+    public static class Department
+    {
+        public const string ObjectName = "department";
+
+        public static class Relations
+        {
+            public const string Faculty = "faculty";
+            public const string Admin = "admin";
+            public const string ProfOrientator = "proforientator";
+            public const string Methodist = "methodist";
+            public const string Teacher = "teacher";
+            public const string Viewer = "viewer";
+        }
     }
 }
